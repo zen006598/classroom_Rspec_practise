d3c6af3135c1bdda0e5ba2fb55184f63483bb760
Jeff Chen <zen00326598@gmail.com>
2023-02-15 12:45:21 +0800

    controller test new action

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -26,4 +26,22 @@ RSpec.describe CoursesController, type: :controller do
       expect(response).to render_template('show')
     end
   end
+
+  describe 'GET new' do
+    it 'assign @couse' do
+      course = build(:course)
+      
+      get :new
+
+      expect(assigns(:course)).to be_a_new(Course)
+    end
+
+    it 'render template' do
+      course = build(:course)
+
+      get :new
+  
+      expect(response).to render_template("new")
+    end
+  end
 end
